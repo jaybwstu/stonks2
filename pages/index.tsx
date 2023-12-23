b62de8586ecb9ff7227672b2a49f519601a19fa2
jaybwstu <75269155+jaybwstu@users.noreply.github.com>
2023-12-23 15:12:25 +0000

    5

--- pages/index.tsx
+++ pages/index.tsx
@@ -212,7 +212,7 @@ export default function Home() {
                 <Flex justifyContent="flex-end" >
                   <Box background={"teal.100"} borderRadius={"5px"} minWidth={"50px"} p={2} >
                     <VStack >
-                      <Text >Available NFTs:</Text>
+                      <Text flexDirection='row'>Available NFTs:</Text>
                       <Text fontWeight={"semibold"}>{Number(candyMachine?.data.itemsAvailable) - Number(candyMachine?.itemsRedeemed)}/{Number(candyMachine?.data.itemsAvailable)}</Text>
                     </VStack>
                   </Box>
